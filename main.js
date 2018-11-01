3dbc2484c1fbb416055119f94246525c68be69db
coryrgriffith <corygriffith9@gmail.com>
2018-11-01 11:58:55 -0500

    add class, subclass, and module examples

--- main.js
+++ main.js
@@ -65,22 +65,63 @@
 
 // DESTRUCTURING
 
-const profile = {
-  name: "John Doe",
-  address: {
-    street: "40 Main St",
-    city: "Boston"
-  },
-  hobbies: ["movies", "music"]
-};
+// const profile = {
+//   name: "John Doe",
+//   address: {
+//     street: "40 Main St",
+//     city: "Boston"
+//   },
+//   hobbies: ["movies", "music"]
+// };
 
-const { name, address, hobbies } = profile;
-const { street, city } = profile.address;
+// const { name, address, hobbies } = profile;
+// const { street, city } = profile.address;
 
-console.log(street, city);
+// console.log(street, city);
 
 // CLASSES
 
-// SUBCLASSES
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   greet() {
+//     return `Hello, my name is ${this.name} and I am ${this.age}`;
+//   }
+// }
+
+// const person1 = new Person("John", 33);
+// const person2 = new Person("Sara", 28);
+
+// console.log(person1.greet());
+
+// // SUBCLASSES
+
+// class Customer extends Person {
+//   constructor(name, age, balance) {
+//     super(name, age);
+//     this.balance = balance;
+//   }
+
+//   info() {
+//     return `${this.name} owes $${this.balance}.00`;
+//   }
+// }
+
+// const customer1 = new Customer("Kevin", 32, 300);
+// console.log(customer1.info());
 
 // MODULES
+
+// file 1 (file1.js)
+
+// export const name = "Jeff";
+// export const nums = [1, 2, 3];
+// export default Person;
+
+// // file 2 (file2.js)
+
+// import { name, nums } from "./file1";
+// import Person from "./file1";
